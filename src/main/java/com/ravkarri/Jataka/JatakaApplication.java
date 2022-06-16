5576e340ebed6b0eb37cd67eb1f19b70f4500870
Ravi Shanker Reddy Karri <me@ravikarri.in>
2022-06-16 18:39:32 +0530

    Migrating to Spring boot

---
+++ src/main/java/com/ravkarri/Jataka/JatakaApplication.java
@@ -0,0 +1,13 @@
+package com.ravkarri.Jataka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravkarri/Jataka/JatakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravkarri.Jataka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
